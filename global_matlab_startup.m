185696fcf6f60dce0bde664930d7e2209972bffa
dll <dll@indiana.edu>
2017-04-11 16:00:01 -0400

    added documentation to global matlab startup and removed the setenv command, this is wrapped up in to the record_variable itself

---
+++ global_matlab_startup.m
@@ -0,0 +1,6 @@
+% every computer will execute a startup.m in the initial startup folder. This file does runs the following,
+%
+% addpath(genpath('r:\repository\multi-lib\'));
+% matlab_startup
+% 
+% therefore, if you want all computers to run additional startup commands, place them here.
--- matlab_startup.m
+++
@@ -1,2 +0,0 @@
-matlab_user = input('enter IU username: ', 's');
-setenv('matlab_user', matlab_user);
