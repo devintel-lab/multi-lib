b67472664a3eec76bfd374997e34be7f3ece2eb6
dll <dll@indiana.edu>
2017-05-03 15:05:59 -0400

    added try catch block per subject and output error to file1

--- data-vis/vis_streams_multiwork.m
+++ data-vis/vis_streams_multiwork.m
@@ -25,37 +25,42 @@ end
 [subs,subtable,subpaths] = cIDs(subexpIDs);
 
 for s = 1:numel(subs)
-    filenames = cell(numel(vars), 1);
-    for v = 1:numel(vars)
-        if ischar(vars{v})
-            filenames{v,1} = fullfile(subpaths{s}, [vars{v} '.mat']);
-        else
-            filenames{v,1} = vars{v};
+    try
+        filenames = cell(numel(vars), 1);
+        for v = 1:numel(vars)
+            if ischar(vars{v})
+                filenames{v,1} = fullfile(subpaths{s}, [vars{v} '.mat']);
+            else
+                filenames{v,1} = vars{v};
+            end
         end
-    end
-    if isfield(args, 'window_times_variable')
-        window_times_file = fullfile(subpaths{s}, [args.window_times_variable '.mat']);
-    elseif ismember(subtable(s,2), [12])
-        window_times_file = [];
-    else
-        window_times_file = fullfile(subpaths{s}, 'cevent_trials.mat');
-    end
-    flag_dir = 1;
-    if exist('directory', 'var') && ~isempty(directory)
-        if exist(directory, 'dir')
-            savefilename = fullfile(directory, sprintf('%d.png', subs(s)));
+        if isfield(args, 'window_times_variable')
+            window_times_file = fullfile(subpaths{s}, [args.window_times_variable '.mat']);
+        elseif ismember(subtable(s,2), [12])
+            window_times_file = [];
         else
-            error('%s does not exist', directory);
+            window_times_file = fullfile(subpaths{s}, 'cevent_trials.mat');
         end
-    else
-        savefilename = [];
-        flag_dir = 0;
-    end
-    
-    args.titlelabel = sprintf('%d', subs(s));
-    h = vis_streams_files(filenames, window_times_file, savefilename, streamlabels, args);
-    if flag_dir
-        close(h);
+        flag_dir = 1;
+        if exist('directory', 'var') && ~isempty(directory)
+            if exist(directory, 'dir')
+                savefilename = fullfile(directory, sprintf('%d.png', subs(s)));
+            else
+                error('%s does not exist', directory);
+            end
+        else
+            savefilename = [];
+            flag_dir = 0;
+        end
+        
+        args.titlelabel = sprintf('%d', subs(s));
+        h = vis_streams_files(filenames, window_times_file, savefilename, streamlabels, args);
+        if flag_dir
+            close(h);
+        end
+    catch ME
+        fprintf(1, '%d : %s', ME.message);
+        continue;
     end
 end
 end
