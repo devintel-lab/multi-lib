31106240d957fef8df00bc3aa72633e7cbc423b0
chenyulab <chenyulab@gmail.com>
2018-09-10 15:22:11 -0400

    Plot the colormap for visualization.

--- data-vis/supporting_functions/set_colors.m
+++ data-vis/supporting_functions/set_colors.m
@@ -1,6 +1,8 @@
 function colors = set_colors(n, is_plot_colormap)
 
 NUM_DEFAULT = 150;
+PLOT_MAX_ROWS = 30;
+
 multisensory_colors = [
          0         0    1.0000
          0    1.0000         0
@@ -96,29 +98,41 @@ if nargin < 2
 end
 
 if is_plot_colormap
-    num_colors = size(colors, 1);
-    h_colormap = figure('Position', [20 20 300 1000], 'Visible', 'off'); % 
+    num_total_colors = size(colors, 1);
+    num_subplots = ceil(num_total_colors/PLOT_MAX_ROWS);
+    h_colormap = figure('Position', [20 20 300*num_subplots 1000]); % , 'Visible', 'off'
     size_unit = 20;
-    hold on;
-    for i = 1:num_colors
-        colorone = colors(i, :);
-        plot_x = [3 3 7 7];
-        upper_y = (num_colors-i+1) * size_unit;
-        lower_y = (num_colors-i) * size_unit+size_unit/10;
-        plot_y = [lower_y upper_y upper_y lower_y];
-        fill(plot_x, plot_y, colorone, 'EdgeColor', 'k');
-        text(mean(plot_x), mean(plot_y), sprintf('%d', i), 'HorizontalAlignment', 'center');
+    
+    for plotidx = 1:num_subplots
+        subplot(1, num_subplots, plotidx);
+        if plotidx < num_subplots
+            num_colors = PLOT_MAX_ROWS;
+        else
+            num_colors = num_total_colors - (plotidx-1)*PLOT_MAX_ROWS;
+        end
+        hold on;
+        for i = 1:num_colors
+            coloridx = i+(plotidx-1)*PLOT_MAX_ROWS;
+            colorone = colors(coloridx, :);
+            plot_x = [3 3 7 7];
+            upper_y = (num_colors-i+1) * size_unit;
+            lower_y = (num_colors-i) * size_unit+size_unit/10;
+            plot_y = [lower_y upper_y upper_y lower_y];
+            fill(plot_x, plot_y, colorone, 'EdgeColor', 'k');
+            text(mean(plot_x), mean(plot_y), sprintf('%d', coloridx), 'HorizontalAlignment', 'center');
+        end
+        xlim([2 8]);
+        ylim([0 (PLOT_MAX_ROWS+1)*size_unit]);
+        % set(gca, 'XTick',[]);
+        % set(gca, 'YTick',[]);
+        set(gca,'Visible','off');
+        hold off;
     end
-    xlim([2 8]);
-    ylim([0 (num_colors+1)*size_unit]);
-    % set(gca, 'XTick',[]);
-    % set(gca, 'YTick',[]);
-    set(gca,'Visible','off');
-    hold off;
-    title_str = sprintf('colormap %d colors', num_colors);
-    text(mean(plot_x), -size_unit, title_str, 'HorizontalAlignment', 'center');
     set(h_colormap,'PaperPositionMode','auto');
+    title_str = sprintf('colormap %d colors', num_total_colors);
+%     text(mean(plot_x), -size_unit, title_str, 'HorizontalAlignment', 'center');
     saveas(h_colormap, [title_str '.png']);
+    fprintf('Colormap saved as %s\n', [title_str '.png']);
 end
 
 end
