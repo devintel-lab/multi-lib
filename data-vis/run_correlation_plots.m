ac155e3a8ae6ae24c957b9807038aae934124826
DianZhi <dianzhi91@126.com>
2020-10-22 02:47:01 -0400

    Update run_correlation_plots.m

--- data-vis/run_correlation_plots.m
+++ data-vis/run_correlation_plots.m
@@ -54,7 +54,7 @@ switch option
             };
         for y = 1:numel(yvars)
             try
-                draw_correlation_plots_v2(subs, yvars{y}, cevmeasures, directory, yvars{y}, yargs);
+                draw_correlation_plots(subs, yvars{y}, cevmeasures, directory, yvars{y}, yargs);
             catch ME
                 disp(ME.message);
                 continue
@@ -80,7 +80,7 @@ switch option
             };
         for y = 1:numel(yvars)
             try
-                draw_correlation_plots_v2(subs, yvars{y}, contmeasures, directory, yvars{y}, yargs);
+                draw_correlation_plots(subs, yvars{y}, contmeasures, directory, yvars{y}, yargs);
             catch ME
                 disp(ME.message);
                 continue
@@ -106,13 +106,13 @@ switch option
         for y = 1:numel(yvars)
             try
                 yargs.categories = 4;
-                draw_correlation_plots_v2({subs, 12}, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
+                draw_correlation_plots({subs, 12}, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
                 yargs.categories = 25;
-                draw_correlation_plots_v2(12, yvars{y}, cevmeasures, directory, [nametags{y} '_toyroom'], yargs);
-                draw_correlation_plots_v2(58, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
-                draw_correlation_plots_v2(59, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
+                draw_correlation_plots(12, yvars{y}, cevmeasures, directory, [nametags{y} '_toyroom'], yargs);
+                draw_correlation_plots(58, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
+                draw_correlation_plots(59, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
                 yargs.categories = 11;
-                draw_correlation_plots_v2(15, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
+                draw_correlation_plots(15, yvars{y}, cevmeasures, directory, nametags{y}, yargs);
             catch ME
                 disp(ME.message);
                 continue
